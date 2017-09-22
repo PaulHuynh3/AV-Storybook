54c53f9d490f40b7d932f849cc608ea4b3dff66a
Paul <paul.huynh3@gmail.com>
2017-09-22 17:59:09 -0400

    record functionality

--- AV Storybook/AV Storybook/StoryPartViewController.m
+++ AV Storybook/AV Storybook/StoryPartViewController.m
@@ -19,7 +19,7 @@
 
 
 @end
-
+//tapping image view plays the person's recording
 @implementation StoryPartViewController
 
 - (void)viewDidLoad {
@@ -33,62 +33,25 @@
     
 }
 - (IBAction)toggleMicrophone:(UIButton *)sender {
+    if ([self.recorder isRecording]){
+        [self.recorder stop];
+        //changes the microphone title to record.
+        [sender setTitle:@"Record" forState:UIControlStateNormal];
+        return;
+    }
+    [sender setTitle:@"Stop" forState:UIControlStateNormal];
     
+    NSError *err = nil;
+    self.recorder = [[AVAudioRecorder alloc]initWithURL:self.audioFileURL settings:@{AVFormatIDKey: @(kAudioFormatMPEG4AAC),
+                                                                                     AVNumberOfChannelsKey: @(2),
+                                                                                     AVSampleRateKey: @(44100)} error:&err];
     
+    if (err != nil){
+        NSLog(@"Error creating recorder: %@", err.localizedDescription);
+        abort();
+    }
+    [self.recorder record];
 }
-
-
-/*
- 
- - (IBAction)toggleRecord:(UIButton *)sender {
- if ([self.recorder isRecording]) {
- [self.recorder stop];
- [sender setTitle:@"Record" forState:UIControlStateNormal];
- return;
- }
- [sender setTitle:@"Stop" forState:UIControlStateNormal];
- 
- NSError *err = nil;
- self.recorder = [[AVAudioRecorder alloc]
- initWithURL:self.audioFileURL
- settings:@{AVFormatIDKey: @(kAudioFormatMPEG4AAC),
- AVNumberOfChannelsKey: @(2),
- AVSampleRateKey: @(44100)}
- error:&err];
- if (err != nil) {
- NSLog(@"Error creating recorder: %@", err.localizedDescription);
- abort();
- }
- [self.recorder record];
- }
- 
- - (IBAction)togglePlay:(UIButton *)sender {
- if ([self.player isPlaying]) {
- [self.player stop];
- [sender setTitle:@"Play" forState:UIControlStateNormal];
- return;
- }
- [sender setTitle:@"Stop" forState:UIControlStateNormal];
- 
- NSError *err = nil;
- self.player = [[AVAudioPlayer alloc]
- initWithContentsOfURL:self.audioFileURL
- error:&err];
- if (err != nil) {
- NSLog(@"Error creating player: %@", err.localizedDescription);
- abort();
- }
- 
- [self.player play];
- }
-
- 
- 
- 
- */
-
-
-
 
 
 
@@ -128,22 +91,39 @@
 
 
 
-
-- (IBAction)userTap:(UITapGestureRecognizer *)sender {
-    
+//responsible for playing the recording of the user as the user taps the picture.
+- (IBAction)playAudioTap:(UITapGestureRecognizer *)sender {
     
     
 }
 
 
-
-
-
-
-/* 
- Your part controller class should have one AVAudioPlayer, and one AVAudioRecorder. Think about where you're recording your data to, and how that will scale if you have five view controllers doing the same thing.
+/*
+ - (IBAction)togglePlay:(UIButton *)sender {
+ if ([self.player isPlaying]) {
+ [self.player stop];
+ [sender setTitle:@"Play" forState:UIControlStateNormal];
+ return;
+ }
+ [sender setTitle:@"Stop" forState:UIControlStateNormal];
+ 
+ NSError *err = nil;
+ self.player = [[AVAudioPlayer alloc]
+ initWithContentsOfURL:self.audioFileURL
+ error:&err];
+ if (err != nil) {
+ NSLog(@"Error creating player: %@", err.localizedDescription);
+ abort();
+ }
+ 
+ [self.player play];
+ }
+ 
+ 
+ 
  
  */
+
 
 
 
