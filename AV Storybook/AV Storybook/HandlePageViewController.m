3a78400c29e8112c679ba4820d2afa5369d3e4df
Paul <paul.huynh3@gmail.com>
2017-09-23 19:25:52 -0400

    scroll forward and backward works for 5 pages

--- AV Storybook/AV Storybook/HandlePageViewController.m
+++ AV Storybook/AV Storybook/HandlePageViewController.m
@@ -10,14 +10,15 @@
 #import "StoryPartViewController.h"
 @interface HandlePageViewController ()<UIPageViewControllerDelegate, UIPageViewControllerDataSource>
 @property (nonatomic) NSArray* pageHolderArray;
+@property (nonatomic) NSUInteger currentPage;
+
 @end
 
 @implementation HandlePageViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
+    self.currentPage = 0;
     self.dataSource= self;
     self.delegate = self;
     
@@ -34,19 +35,46 @@
     
     self.pageHolderArray = @[view1, view2, view3, view4, view5];
     
-}
-
-
-- (nullable UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController{
-
-    return nil;
+    
+    self.navigationItem.title = [NSString stringWithFormat:@"Page #1"];
+    
+    [self setViewControllers:@[view1] direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
     
 }
 
 
-- (nullable UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController{
-    return nil;
+//scroll backwards
 
+- (nullable UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController{
+
+    NSInteger currentIndex = [self.pageHolderArray indexOfObject:viewController];
+    NSInteger prevIndex = (currentIndex - 1) % self.pageHolderArray.count;
+    
+    self.navigationItem.title = [NSString stringWithFormat:@"Page #%lu", prevIndex + 1];
+    self.currentPage = prevIndex;
+    return self.pageHolderArray[prevIndex];
+    
+}
+
+
+//scroll forward
+- (nullable UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController{
+    
+    NSInteger currentIndex = [self.pageHolderArray indexOfObject:viewController];
+    NSInteger nextIndex = currentIndex + 1;
+    NSInteger count = self.pageHolderArray.count - 1;
+    
+    //prevent scroll from going out of bounds
+    if (nextIndex > count){
+    self.currentPage = 0;
+    self.navigationItem.title = [NSString stringWithFormat:@"Page #1"];
+    return self.pageHolderArray[self.currentPage];
+    }
+    
+    self.navigationItem.title = [NSString stringWithFormat:@"Page #%lu", nextIndex + 1];
+    self.currentPage = nextIndex;
+    return self.pageHolderArray[nextIndex];
+    
 }
 
 
